0dbdd42d6ce83489f02b59ca888ff85db5a84e5c
Miguel Ricardo Buitrago Rodriguez <miguel.buitrago@nuevaeps.com.co>
2026-05-25 09:36:51 -0500

    Subo CRUD completo Grapql

---
+++ EjercicioM/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace EjercicioM.Models
+{
+    public class Producto
+    {
+
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public decimal? precio { get; set; }
+
+    }
+}
